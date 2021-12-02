fdb973ba5c768ba2afa43a9858deb63ee262e053
gabriel <puppizin@gmail.com>
2021-12-02 15:25:46 -0300

    Initial Commit

---
+++ foca2/foca2App.swift
@@ -0,0 +1,17 @@
+//
+//  foca2App.swift
+//  foca2
+//
+//  Created by Gabriel Puppi on 02/12/21.
+//
+
+import SwiftUI
+
+@main
+struct foca2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
